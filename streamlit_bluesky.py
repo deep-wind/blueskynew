864d34873433d6779d79ea7f335a3ea265fcaf9e
Brindha Manickavasakan <72887609+Brindha-m@users.noreply.github.com>
2024-09-30 05:27:31 +0530

    Update streamlit_bluesky.py

--- streamlit_bluesky.py
+++ streamlit_bluesky.py
@@ -105,21 +105,35 @@ def predict(latitude_input, longitude_input, date):
     ##############################################
     #            PREDICTION MODULE               #
     ##############################################
-    data_frame = pd.read_csv(r"5days_combined.csv")
-    df1 = data_frame.reset_index()['NO2']
-    st.write(df1)
-
-    ### LSTM are sensitive to the scale of the data. so we apply MinMax scaler 
-    df1 = df1.fillna(0)
     
-    scaler = MinMaxScaler(feature_range=(0,1))
-    df1 = scaler.fit_transform(np.array(df1).reshape(-1, 1))
+    ##############################################
+    #            PREDICTION MODULE               #
+    ##############################################
+    ### Data Collection
+    data_frame=pd.read_csv(r"5days_combined.csv")
+    df1=data_frame.reset_index()['NO2']
+    st.write(df1)
+    ### LSTM are sensitive to the scale of the data. so we apply MinMax scaler 
+
+    df1 = df1.fillna(0)
+    ### LSTM are sensitive to the scale of the data. so we apply MinMax scaler 
+
+    scaler=MinMaxScaler(feature_range=(0,1))
+    df1=scaler.fit_transform(np.array(df1).reshape(-1,1))
     
     ##splitting dataset into train and test split
-    training_size = int(len(df1) * 0.70)
-    test_size = len(df1) - training_size
-    train_data, test_data = df1[0:training_size, :], df1[training_size:len(df1), :1]
 
+
+    training_size=int(len(df1)*0.70)
+    test_size=len(df1)-training_size
+    train_data,test_data=df1[0:training_size,:],df1[test_size:len(df1),:1]
+    print(train_data)
+    print(test_data)
+    
+    # st.write(train_data)
+    # st.write(test_data)
+       
+    
     # convert an array of values into a dataset matrix
     def create_dataset(dataset, time_step=1):
      	dataX, dataY = [], []
@@ -131,71 +145,105 @@ def predict(latitude_input, longitude_input, date):
     
     # reshape into X=t,t+1,t+2,t+3 and Y=t+4
     time_step = 1
-    
-    time_step = 1
     X_train, y_train = create_dataset(train_data, time_step)
     X_test, ytest = create_dataset(test_data, time_step)
 
-    X_train = X_train.reshape(X_train.shape[0], X_train.shape[1], 1)
-    X_test = X_test.reshape(X_test.shape[0], X_test.shape[1], 1)
+    # print(X_train.shape), print(y_train.shape)
+    # print(X_test.shape), print(ytest.shape)
+  
+    # reshape input to be [samples, time steps, features] which is required for LSTM
+    
+    X_train =X_train.reshape(X_train.shape[0],X_train.shape[1] , 1)
+    X_test = X_test.reshape(X_test.shape[0],X_test.shape[1] , 1)
 
-    model = Sequential()
-    model.add(LSTM(50, return_sequences=True, input_shape=(1, 1)))
-    model.add(LSTM(50, return_sequences=True))
+    # st.info(X_train.shape)
+    # st.info(y_train.shape)
+    
+    # print(X_test.shape), print(ytest.shape)
+    
+    ### Create the Stacked LSTM model
+    model=Sequential()
+    model.add(LSTM(50,return_sequences=True,input_shape=(1,1)))
+    model.add(LSTM(50,return_sequences=True))
     model.add(LSTM(50))
     model.add(Dense(1))
-    model.compile(loss='mean_squared_error', optimizer='adam')
+    model.compile(loss='mean_squared_error',optimizer='adam')
+    
     
     model.summary()
-    model.fit(X_train, y_train, validation_data=(X_test, ytest), epochs=50, batch_size=8, verbose=1)
+    model.fit(X_train,y_train,validation_data=(X_test,ytest),epochs=50,batch_size=8,verbose=1)
     
-    train_predict = model.predict(X_train)
-    test_predict = model.predict(X_test)
-    
-    train_predict = scaler.inverse_transform(train_predict)
-    test_predict = scaler.inverse_transform(test_predict)
-    
-    math.sqrt(mean_squared_error(y_train, train_predict))
-    math.sqrt(mean_squared_error(ytest, test_predict))
 
-    look_back = 1
+    ### Lets Do the prediction and check performance metrics
+    train_predict=model.predict(X_train)
+    test_predict=model.predict(X_test)
+    
+    ##Transformback to original form
+    train_predict=scaler.inverse_transform(train_predict)
+    test_predict=scaler.inverse_transform(test_predict)
+    
+    ### Calculate RMSE performance metrics
+    
+    math.sqrt(mean_squared_error(y_train,train_predict))
+    
+    ### Test Data RMSE
+    math.sqrt(mean_squared_error(ytest,test_predict))
+    
+    ### Plotting 
+    # shift train predictions for plotting
+    look_back=1
     trainPredictPlot = np.empty_like(df1)
     trainPredictPlot[:, :] = np.nan
-    trainPredictPlot[look_back:len(train_predict) + look_back, :] = train_predict
-    
+    trainPredictPlot[look_back:len(train_predict)+look_back, :] = train_predict
+    # shift test predictions for plotting
     testPredictPlot = np.empty_like(df1)
     testPredictPlot[:, :] = np.nan
-    testPredictPlot[len(train_predict) + (look_back * 2) + 1:len(df1) - 1, :] = test_predict
+    testPredictPlot[len(train_predict)+(look_back*2)+1:len(df1)-1, :] = test_predict
+      
+    x_input=test_data[len(test_data)-1:].reshape(1,-1)
     
-    x_input = test_data[len(test_data) - 1:].reshape(1, -1)
+    temp_input=list(x_input)
+    temp_input=temp_input[0].tolist()
     
-    temp_input = list(x_input)
-    temp_input = temp_input[0].tolist()
+    # demonstrate prediction for next days
     
-    lst_output = []
-    n_steps = 1
-    i = 0
-    while i < predict_days:
-        if len(temp_input) > 1:
-            x_input = np.array(temp_input[1:])
-            x_input = x_input.reshape(1, -1)
+    lst_output=[]
+    n_steps=1
+    i=0
+    while(i<predict_days):
+        
+        if(len(temp_input)>1):
+            #print(temp_input)
+            x_input=np.array(temp_input[1:])
+            print("{} day input {}".format(i,x_input))
+            x_input=x_input.reshape(1,-1)
             x_input = x_input.reshape((1, n_steps, 1))
+            #print(x_input)
             yhat = model.predict(x_input, verbose=0)
+            print("{} day output {}".format(i,yhat))
             temp_input.extend(yhat[0].tolist())
-            temp_input = temp_input[1:]
+            temp_input=temp_input[1:]
+            #print(temp_input)
             lst_output.extend(yhat.tolist())
-            i += 1
+            i=i+1
         else:
-            x_input = x_input.reshape((1, n_steps, 1))
+            x_input = x_input.reshape((1, n_steps,1))
             yhat = model.predict(x_input, verbose=0)
+            print(yhat[0])
             temp_input.extend(yhat[0].tolist())
+            print(len(temp_input))
             lst_output.extend(yhat.tolist())
-            i += 1
+            i=i+1
+        
     
-    no2_output = pd.DataFrame(scaler.inverse_transform(lst_output), columns=['NO2 Concentration'])
+    print(lst_output)
+        
+    # st.write(df3)
+    no2_output=pd.DataFrame(scaler.inverse_transform(lst_output),columns=['NO2 Concentration ðŸ­'])
     st.write(no2_output)
-    output = no2_output.at[predict_days - 1, 'NO2 Concentration']
+    output= (no2_output.at[predict_days-1,'NO2 Concentration­'])
     return output
+
 
 def main():
     st.markdown("<h1 style='color:green; text-align:center; font-family:times new roman; font-weight:bold; font-size:20pt;'>NO2 Prediction</h1>", unsafe_allow_html=True)
