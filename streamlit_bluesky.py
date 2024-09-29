4391584f0cb8561e1c79158fd657370f84ae721a
Brindha Manickavasakan <72887609+Brindha-m@users.noreply.github.com>
2024-09-30 05:13:15 +0530

    Update streamlit_bluesky.py

--- streamlit_bluesky.py
+++ streamlit_bluesky.py
@@ -1,4 +1,3 @@
-# -*- coding: utf-8 -*-
 """
 Created on Mon Oct 31 19:57:21 2022
 
@@ -7,23 +6,34 @@ Created on Mon Oct 31 19:57:21 2022
 import requests
 import streamlit as st
 import pandas as pd
+#import harp
 from netCDF4 import Dataset
 import os
+from os.path import join
+import glob
 import xarray as xr
 import numpy as np
+from numpy import array
+import itertools
 import datetime
+import time
+import math
 from sklearn.metrics import mean_squared_error
 from tensorflow.keras.models import Sequential
-from tensorflow.keras.layers import Dense, LSTM
+from tensorflow.keras.layers import Dense
+from tensorflow.keras.layers import LSTM
 import matplotlib.pyplot as plt 
+from datetime import datetime
 from sklearn.preprocessing import MinMaxScaler
-from streamlit_folium import folium_static, st_folium
+import base64
+from streamlit_folium import folium_static
+from streamlit_folium import st_folium
 import folium
 import gdown
-import math
 
 # specify that TensorFlow performs computations using the CPU
 os.environ['TF_ENABLE_MLIR_OPTIMIZATIONS'] = '1'
+
 
 # BlueSky Above: Pollution estimation using hyper-spectral satellite imagery and maps
 
@@ -33,38 +43,60 @@ local_filename = '5days_combined.nc'
 
 # Check if the file already exists
 if not os.path.exists(local_filename):
-    print(f"File {local_filename} not found locally. Downloading...")
+    print(f"File {local_filename} not found locally. Downloading from Google Drive...")
+    # Download the file from Google Drive
     gdown.download(f"https://drive.google.com/uc?export=download&id={file_id}", local_filename, quiet=True)
 else:
-    print(f"File {local_filename} already exists. Skipping download.")
+    print(f"File {local_filename} already exists locally. Skipping download.")
 
-# Load the dataset
+# Install a pip package in the current Jupyter kernel
+import sys
+
+# Latitude and Longitude boundaries
+LatMin=51.25
+LatMax=51.75
+LngMin=-0.6
+LngMax=0.28
+from sentinelsat import SentinelAPI, geojson_to_wkt
+import json
+
+geojsonstring='{{"type":"FeatureCollection","features":[{{"type":"Feature","properties":{{}},"geometry":{{"type":"Polygon","coordinates":[[[{LongiMin},{LatiMin}],[{LongiMax},{LatiMin}],[{LongiMax},{LatiMax}],[{LongiMin},{LatiMax}],[{LongiMin},{LatiMin}]]]}}}}]}}'.format(LongiMin=LngMin,LatiMin=LatMin,LongiMax=LngMax,LatiMax=LatMax)
+
+# Sentinel API setup
+api = SentinelAPI('s5pguest', 's5pguest' , 'https://s5phub.copernicus.eu/dhus')
+footprint = geojson_to_wkt(json.loads(geojsonstring))
+
+startdate='20240901'
+enddate='20220905'
+
 L3_data1 = Dataset(local_filename)
-print("Dataset variables loaded:", L3_data1.variables.keys())
+print("Dataset loaded successfully.")
+print("Available variables in the dataset:", L3_data1.variables.keys())
+
 lat = L3_data1.variables['latitude'][:]
 lon = L3_data1.variables['longitude'][:]
 time_data = L3_data1.variables['time'][:]
 no2 = L3_data1.variables['tropospheric_NO2_column_number_density'][:,:,:]
 
 def predict(latitude_input, longitude_input, date):
-    predict_days = abs((datetime.datetime.strptime('2024-09-05', "%Y-%m-%d") - datetime.datetime.strptime(date, "%Y-%m-%d")).days)
-    st.write("Days to predict:", predict_days)
+    predict_days = abs((datetime.strptime('2024-09-05',"%Y-%m-%d") - datetime.strptime(date,"%Y-%m-%d")).days)
+    st.write("Predict days:", predict_days)
 
-    # Finding the closest lat and lon indices
     sq_diff_lat = (lat - latitude_input) ** 2
     sq_diff_lon = (lon - longitude_input) ** 2
+    
     min_index_lat = sq_diff_lat.argmin()
     min_index_lon = sq_diff_lon.argmin()
-
+    
     start_date = L3_data1.variables['time'].units[14:24]
     end_date = L3_data1.variables['time'].units[14:18] + '-09-05'
     
     date_range = pd.date_range(start=start_date, end=end_date)
-    st.success("Date range of the data: " + str(date_range))
+    st.success(f"Date range: {date_range}")
     
     df = pd.DataFrame(0, columns=['NO2'], index=date_range)
+    
     dt = np.arange(0, 4)
-
     for i in dt:
         df.iloc[i] = no2[i, min_index_lat, min_index_lon]
         
@@ -73,29 +105,22 @@ def predict(latitude_input, longitude_input, date):
     ##############################################
     #            PREDICTION MODULE               #
     ##############################################
-    # Load the data for prediction
     data_frame = pd.read_csv(r"5days_combined.csv")
     df1 = data_frame.reset_index()['NO2']
-    st.write("Original NO2 data:", df1)
+    st.write(df1)
     
     df1 = df1.fillna(0)
     
-    # Scale the data
-    scaler = MinMaxScaler(feature_range=(0, 1))
+    scaler = MinMaxScaler(feature_range=(0,1))
     df1 = scaler.fit_transform(np.array(df1).reshape(-1, 1))
     
-    # Splitting the dataset into training and testing sets
     training_size = int(len(df1) * 0.70)
     test_size = len(df1) - training_size
-    train_data, test_data = df1[0:training_size, :], df1[test_size:len(df1), :1]
-    
-    print("Train data size:", train_data.shape)
-    print("Test data size:", test_data.shape)
-    
-    # Prepare the dataset for LSTM
+    train_data, test_data = df1[0:training_size, :], df1[training_size:len(df1), :1]
+
     def create_dataset(dataset, time_step=1):
         dataX, dataY = [], []
-        for i in range(len(dataset) - time_step - 1):         
+        for i in range(len(dataset) - time_step - 1):
             a = dataset[i:(i + time_step), 0]
             dataX.append(a)
             dataY.append(dataset[i + time_step, 0])
@@ -105,11 +130,9 @@ def predict(latitude_input, longitude_input, date):
     X_train, y_train = create_dataset(train_data, time_step)
     X_test, ytest = create_dataset(test_data, time_step)
 
-    # Reshape input to be [samples, time steps, features]
     X_train = X_train.reshape(X_train.shape[0], X_train.shape[1], 1)
     X_test = X_test.reshape(X_test.shape[0], X_test.shape[1], 1)
 
-    # Build the LSTM model
     model = Sequential()
     model.add(LSTM(50, return_sequences=True, input_shape=(1, 1)))
     model.add(LSTM(50, return_sequences=True))
@@ -117,41 +140,29 @@ def predict(latitude_input, longitude_input, date):
     model.add(Dense(1))
     model.compile(loss='mean_squared_error', optimizer='adam')
     
-    print("Training model summary:")
     model.summary()
-
     model.fit(X_train, y_train, validation_data=(X_test, ytest), epochs=50, batch_size=8, verbose=1)
     
-    # Make predictions
     train_predict = model.predict(X_train)
     test_predict = model.predict(X_test)
     
-    # Inverse transform the predictions
     train_predict = scaler.inverse_transform(train_predict)
     test_predict = scaler.inverse_transform(test_predict)
     
-    st.info("Train predictions complete")
-    st.info("Test predictions complete")
-    ### Calculate RMSE performance metrics
-    
-    math.sqrt(mean_squared_error(y_train,train_predict))
-    
-    ### Test Data RMSE
-    math.sqrt(mean_squared_error(ytest,test_predict))
-    
-    ### Plotting 
-    # shift train predictions for plotting
-    look_back=1
+    math.sqrt(mean_squared_error(y_train, train_predict))
+    math.sqrt(mean_squared_error(ytest, test_predict))
+
+    look_back = 1
     trainPredictPlot = np.empty_like(df1)
     trainPredictPlot[:, :] = np.nan
-    trainPredictPlot[look_back:len(train_predict)+look_back, :] = train_predict
-    # shift test predictions for plotting
+    trainPredictPlot[look_back:len(train_predict) + look_back, :] = train_predict
+    
     testPredictPlot = np.empty_like(df1)
     testPredictPlot[:, :] = np.nan
-    testPredictPlot[len(train_predict)+(look_back*2)+1:len(df1)-1, :] = test_predict
-   
-    # Future predictions
+    testPredictPlot[len(train_predict) + (look_back * 2) + 1:len(df1) - 1, :] = test_predict
+    
     x_input = test_data[len(test_data) - 1:].reshape(1, -1)
+    
     temp_input = list(x_input)
     temp_input = temp_input[0].tolist()
     
@@ -174,14 +185,15 @@ def predict(latitude_input, longitude_input, date):
             temp_input.extend(yhat[0].tolist())
             lst_output.extend(yhat.tolist())
             i += 1
-
+    
     no2_output = pd.DataFrame(scaler.inverse_transform(lst_output), columns=['NO2 Concentration'])
     st.write(no2_output)
     output = no2_output.at[predict_days - 1, 'NO2 Concentration']
     return output
 
 def main():
-    st.markdown("<h1 style ='color:green; text_align:center;font-family:times new roman;font-weight: bold;font-size:20pt;'>NO2 PREDICTION </h1>", unsafe_allow_html=True)
+    st.markdown("<h1 style='color:green; text-align:center; font-family:times new roman; font-weight:bold; font-size:20pt;'>NO2 Prediction</h1>", unsafe_allow_html=True)
+    
     m = folium.Map()
     m.add_child(folium.LatLngPopup())
     map = st_folium(m, height=500, width=700)
@@ -191,19 +203,17 @@ def main():
         st.write("Selected Latitude:", latitude_input)
         st.write("Selected Longitude:", longitude_input)
     except:
-        st.warning("No location chosen")
-
+        st.warning("No location selected.")
+    
     date = st.date_input('Date', value=pd.to_datetime('2024-09-06'), min_value=pd.to_datetime('2024-09-06'), max_value=pd.to_datetime('2025-09-06'))
     
     if st.button("Predict"):
-        st.success("Latitude: " + str(latitude_input))
-        st.success("Longitude: " + str(longitude_input))
+        latitude_input = float(latitude_input)
+        longitude_input = float(longitude_input)
         date = str(date)
-        st.success("Selected date: " + date)
-        with st.spinner("Predicting the results..."):
-            result = predict(latitude_input, longitude_input, date)
-        st.success(f'Predicted NO2 Concentration is {round(result, 4)} molecules/cm2')
-        st.balloons()
+        st.success(f"Predicting NO2 level for the location (Lat: {latitude_input}, Lon: {longitude_input}) on {date}")
+        output = predict(latitude_input, longitude_input, date)
+        st.success(f"NO2 level predicted: {output} mol/m²")
 
-if __name__ == "__main__":
+if __name__ == '__main__':
     main()
