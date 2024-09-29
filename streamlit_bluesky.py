c401f1fb1e0ff25a664cc2dad9a34b1c7baeab2b
Brindha Manickavasakan <72887609+Brindha-m@users.noreply.github.com>
2024-09-30 05:23:22 +0530

    Update streamlit_bluesky.py

--- streamlit_bluesky.py
+++ streamlit_bluesky.py
@@ -108,23 +108,29 @@ def predict(latitude_input, longitude_input, date):
     data_frame = pd.read_csv(r"5days_combined.csv")
     df1 = data_frame.reset_index()['NO2']
     st.write(df1)
-    
+
+    ### LSTM are sensitive to the scale of the data. so we apply MinMax scaler 
     df1 = df1.fillna(0)
     
     scaler = MinMaxScaler(feature_range=(0,1))
     df1 = scaler.fit_transform(np.array(df1).reshape(-1, 1))
     
+    ##splitting dataset into train and test split
     training_size = int(len(df1) * 0.70)
     test_size = len(df1) - training_size
     train_data, test_data = df1[0:training_size, :], df1[training_size:len(df1), :1]
 
+    # convert an array of values into a dataset matrix
     def create_dataset(dataset, time_step=1):
-        dataX, dataY = [], []
-        for i in range(len(dataset) - time_step - 1):
-            a = dataset[i:(i + time_step), 0]
-            dataX.append(a)
-            dataY.append(dataset[i + time_step, 0])
-        return np.array(dataX), np.array(dataY)
+     	dataX, dataY = [], []
+     	for i in range(len(dataset)-time_step-1):         
+        		a = dataset[i:(i+time_step), 0]   ###i=0, 0,1,2,3-----99   100 
+        		dataX.append(a)
+        		dataY.append(dataset[i + time_step, 0])
+     	return np.array(dataX), np.array(dataY)
+    
+    # reshape into X=t,t+1,t+2,t+3 and Y=t+4
+    time_step = 1
     
     time_step = 1
     X_train, y_train = create_dataset(train_data, time_step)
