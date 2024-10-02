b4feb1e588046ddf70a9103569ea01e7103680aa
Brindha Manickavasakan <72887609+Brindha-m@users.noreply.github.com>
2024-10-03 02:41:58 +0530

    Update streamlit_bluesky.py

--- streamlit_bluesky.py
+++ streamlit_bluesky.py
@@ -18,7 +18,7 @@ import itertools
 import datetime
 import time
 import math
-import tensorflow
+from tensorflow.keras import models
 from sklearn.metrics import mean_squared_error
 from tensorflow.keras.models import Sequential
 from tensorflow.keras.layers import Dense
