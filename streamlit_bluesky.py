98c2d57748ef2679bbc1cb71f66fc045e4e6ecc5
deep-wind <78983104+deep-wind@users.noreply.github.com>
2024-11-18 04:00:40 +0530

    Update streamlit_bluesky.py

--- streamlit_bluesky.py
+++ streamlit_bluesky.py
@@ -34,9 +34,6 @@ import gdown
 import plotly.express as px
 
 # specify that TensorFlow performs computations using the CPU
-os.environ['TF_ENABLE_MLIR_OPTIMIZATIONS'] = '1'
-
-os.environ["CUDA_VISIBLE_DEVICES"] = "0"
 
 # BlueSky Above: Pollution estimation using hyper-spectral satellite imagery and maps
 
